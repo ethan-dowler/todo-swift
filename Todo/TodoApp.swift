2121ddca98d0e72e16a689a19feeed97ab73aa64
Ethan Dowler <thomas.e.dowler@uscis.dhs.gov>
2023-06-20 15:27:56 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Dowler, Thomas E (CTR) on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
